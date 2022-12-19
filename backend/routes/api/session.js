1339b951ac70d9af929adc3f747d9125e45ae06c
Samuel Park <s.park111497@gmail.com>
2022-12-19 13:41:56 -0800

    finished backend

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { handleValidationErrors } = require("../../utils/validation");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password"),
+  handleValidationErrors,
+];
+
 // Route handler for logging in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -21,9 +35,6 @@ router.post(
       const err = new Error("Login failed");
       err.status = 401;
       err.title = "Login failed";
-      err.errors = [
-        "An account with the provided username/email and password does not exist.",
-      ];
       return next(err);
     }
 
