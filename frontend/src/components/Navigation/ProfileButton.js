0da019fc7cadcebb59e9f17eeb13ba15d3deb31d
Samuel Park <s.park111497@gmail.com>
2022-12-23 14:58:19 -0800

    added user icon with a dropdown menu containing a logout button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -4,12 +4,60 @@ import * as sessionActions from '../../store/session';
 
 const ProfileButton = ({ user }) => {
   const dispatch = useDispatch();
-  const [menu, setMenu] = useState(false);
+  const [isDisplayed, setIsDisplayed] = useState(false);
+
+  const handleDropdown = () => {
+    if (isDisplayed) return;
+    setIsDisplayed(true);
+  };
+
+  const logout = (e) => {
+    e.preventDefault();
+
+    dispatch(sessionActions.logoutUser());
+  };
+
+  useEffect(() => {
+    if (!isDisplayed) return;
+
+    const closeMenu = (e) => {
+      // let isDropdown = false;
+      // let currNode = e.target;
+      // const page = document.querySelector('body');
+
+      // while (currNode !== page) {
+      //   if (currNode.className === 'dropdown') {
+      //     isDropdown = true;
+      //     break;
+      //   }
+
+      //   currNode = currNode.parentNode;
+      // }
+
+      // if (!isDropdown) setIsDisplayed(false);
+      setIsDisplayed(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [isDisplayed]);
 
   return (
-    <button>
-      <i className='fas fa-user-circle' />
-    </button>
+    <>
+      <button className='dropdown' onClick={handleDropdown}>
+        <i className='fas fa-user-circle' />
+      </button>
+      {isDisplayed && (
+        <ul className='dropdown'>
+          <li>{user.username}</li>
+          <li>{user.email}</li>
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
   );
 };
 
